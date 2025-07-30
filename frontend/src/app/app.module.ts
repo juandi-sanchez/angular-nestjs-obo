a564d618982c46fcbc1c2486acd47388a8b4f2a5
juandi-sanchez <114146833+juandi-sanchez@users.noreply.github.com>
2025-07-30 14:11:14 +0200

    Update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,42 +1,81 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { MsalModule, MsalRedirectComponent } from '@azure/msal-angular';
-import { PublicClientApplication } from '@azure/msal-browser';
+import {
+  MsalModule,
+  MsalGuardConfiguration,
+  MsalInterceptorConfiguration,
+  MSAL_GUARD_CONFIG,
+  MSAL_INSTANCE,
+  MSAL_INTERCEPTOR_CONFIG,
+  MsalService,
+  MsalGuard,
+  MsalBroadcastService,
+} from '@azure/msal-angular';
+import { IPublicClientApplication, PublicClientApplication, InteractionType } from '@azure/msal-browser';
 import { AppComponent } from './app.component';
+
+const tenantId = 'YOUR_TENANT_ID';
+const clientId = 'YOUR_CLIENT_ID';
+const backendClientId = 'YOUR_BACKEND_CLIENT_ID';
+
+function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication({
+    auth: {
+      clientId,
+      authority: `https://login.microsoftonline.com/${tenantId}`,
+      redirectUri: '/',
+    },
+    cache: {
+      cacheLocation: 'localStorage',
+      storeAuthStateInCookie: false,
+    },
+  });
+}
+
+function MSALGuardConfigFactory(): MsalGuardConfiguration {
+  return {
+    interactionType: InteractionType.Popup,
+    authRequest: {
+      scopes: ['user.read'],
+    },
+  };
+}
+
+function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
+  const protectedResourceMap = new Map<string, Array<string>>();
+  protectedResourceMap.set('http://localhost:3000/api/protected', [`api://${backendClientId}/access_as_user`]);
+
+  return {
+    interactionType: InteractionType.Popup,
+    protectedResourceMap,
+  };
+}
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
-    MsalModule.forRoot(
-      new PublicClientApplication({
-        auth: {
-          clientId: 'YOUR_CLIENT_ID',
-          authority: 'https://login.microsoftonline.com/YOUR_TENANT_ID',
-          redirectUri: '/',
-        },
-        cache: {
-          cacheLocation: 'localStorage',
-          storeAuthStateInCookie: false,
-        },
-      }),
-      {
-        interactionType: 'popup',
-        authRequest: {
-          scopes: ['user.read'],
-        },
-      },
-      {
-        interactionType: 'popup',
-        protectedResourceMap: new Map([
-          ['http://localhost:3000/api/protected', ['api://YOUR_BACKEND_CLIENT_ID/access_as_user']],
-        ]),
-      }
-    ),
+    MsalModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent, MsalRedirectComponent],
+  providers: [
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory,
+    },
+    {
+      provide: MSAL_GUARD_CONFIG,
+      useFactory: MSALGuardConfigFactory,
+    },
+    {
+      provide: MSAL_INTERCEPTOR_CONFIG,
+      useFactory: MSALInterceptorConfigFactory,
+    },
+    MsalService,
+    MsalGuard,
+    MsalBroadcastService,
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
