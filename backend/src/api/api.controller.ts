af81689da34d55075a188d303782f694d526650b
juandi-sanchez <114146833+juandi-sanchez@users.noreply.github.com>
2025-07-30 14:15:36 +0200

    Update api.controller.ts

--- backend/src/api/api.controller.ts
+++ backend/src/api/api.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt.guard';
+import { Request } from 'express';
 
 @Controller('api')
 export class ApiController {
   @UseGuards(JwtAuthGuard)
   @Get('protected')
-  getProtected(@Req() req) {
+  getProtected(@Req() req: Request) {
     return {
       message: 'Contenido protegido del API',
       user: req.user
